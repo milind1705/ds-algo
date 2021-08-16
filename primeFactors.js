5f5e592a14daebdc3f5062a8c41773bb16e5a525
unknown <sois.milindsir@gmail.com>
2021-08-16 22:06:16 +0530

    prime factors

--- primeFactors.js
+++ primeFactors.js
@@ -1,11 +1,31 @@
-function prime (n){
+// function prime (n){
    
-   for(let i=n; i>=1; i--){
-if(n % i==0) {
+//    for(let i=n; i>=1; i--){
+// if(n % i==0) {
 
-    console.log(i)
-}
-   }
-}
+//     console.log(i)
+// }
+//    }
+// }
 
-prime(100);
+// prime(100);
+
+function primeFactors(n){
+   let divisor = 2;
+   let factors = [];
+
+
+   while(n>2){
+      if(n % divisor == 0){
+         factors.push(divisor); 
+         n= n/ divisor;
+      }
+      else{
+        divisor++;
+      }     
+    }
+    console.log (factors);
+  
+};
+
+primeFactors(24);
