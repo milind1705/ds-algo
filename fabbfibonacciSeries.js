d9a7ada8a75f570e42493e148fcd2b3a6c29baa4
unknown <sois.milindsir@gmail.com>
2021-08-17 21:14:48 +0530

    fabbfibonacci series

---
+++ fabbfibonacciSeries.js
@@ -0,0 +1,19 @@
+function fibonacci(n){
+    var fibo = [0, 1];
+    if (n < 2) return 1;
+    for(let i = 2; i <= n; i++ ){
+        fibo[i]= fibo[i-1]+fibo[i-2];
+    } 
+    console.log (fibo[n])
+    return fibo[n]
+};
+
+fibonacci(12);
+
+//the series in which the number is addition of previce two number.
+
+//second try
+
+function fibonacci(n){
+    
+}
