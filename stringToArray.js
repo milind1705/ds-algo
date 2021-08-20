75f90d858cb919347c0ce1cb5346e7f93957bd55
unknown <sois.milindsir@gmail.com>
2021-08-20 22:44:04 +0530

    string to array

---
+++ stringToArray.js
@@ -0,0 +1,15 @@
+// function strToArray(str) {
+
+//   var splitString = str.split(" ");
+
+//   return splitString;
+// }
+
+// console.log(strToArray("you are not used"));
+
+function arrayToString(arr) {
+    var makeString = arr.toString();
+    return makeString;
+  }
+  console.log(arrayToString(["yuu", "change"]));
+  
